1c0d75be9420f4c2305b24bf8c21456c71dc59ff
Bubs38 <burgain.aurelien@gmail.com>
2022-06-18 16:08:30 +0200

    Version 4

--- scripts/app.js
+++ scripts/app.js
@@ -7,15 +7,29 @@ btnNav.addEventListener('click', () => {
 
   listeNav.classList.toggle('active-nav');
 
-  if(imgBtn.src.includes('menu')){
-    imgBtn.src = './ressources/croix.svg';
-    imgBtn.style.width = "30px";
-    imgBtn.style.height = "30px";
+  if(window.location.href.includes('index')) {
+    if(imgBtn.src.includes('menu')){
+      imgBtn.src = './ressources/croix.svg';
+      imgBtn.style.width = "30px";
+      imgBtn.style.height = "30px";
+    }
+    else{
+      imgBtn.src = './ressources/menu.svg';
+      imgBtn.style.width = "40px";
+      imgBtn.style.height = "40px";
+    }
   }
-  else{
-    imgBtn.src = './ressources/menu.svg';
-    imgBtn.style.width = "40px";
-    imgBtn.style.height = "40px";
+  else {
+    if(imgBtn.src.includes('menu')){
+      imgBtn.src = '../ressources/croix.svg';
+      imgBtn.style.width = "30px";
+      imgBtn.style.height = "30px";
+    }
+    else{
+      imgBtn.src = '../ressources/menu.svg';
+      imgBtn.style.width = "40px";
+      imgBtn.style.height = "40px";
+    }
   }
 
 });
