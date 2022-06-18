7ecdfba65ed9bee89b1de642a9109cd985905b50
Bubs38 <burgain.aurelien@gmail.com>
2022-06-18 17:17:57 +0200

    Version finale

--- scripts/app.js
+++ scripts/app.js
@@ -60,3 +60,29 @@ allCross.forEach(cross => {
     } 
   })
 })
+
+
+
+// Onglets tableaux
+
+const choixTarifs = Array.from(document.querySelectorAll('.choix'));
+const contenuTarifs = Array.from(document.querySelectorAll('.panneau'));
+
+choixTarifs.forEach (choix => {
+  choix.addEventListener('click', (e) => {
+
+    let indexClic = choixTarifs.indexOf(e.target);
+
+    for (i = 0; i < choixTarifs.length; i++) {
+
+      if (i === indexClic){
+        choixTarifs[i].classList.add('active-choix');
+        contenuTarifs[i].classList.add('panneau-active');
+      } else {
+        choixTarifs[i].classList.remove('active-choix');
+        contenuTarifs[i].classList.remove('panneau-active');
+      }
+
+    }
+  })
+})
